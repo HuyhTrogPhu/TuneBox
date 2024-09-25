c497c0eee0d2eda69e64c894fe9d3d60fba38504
Your Name <you@example.com>
2024-09-25 16:45:11 +0700

    controller category, brand, instrument

--- TuneBox/EcommerceAdmin/src/main/java/org/example/ecommerceadmin/controller/BrandController.java
+++ TuneBox/EcommerceAdmin/src/main/java/org/example/ecommerceadmin/controller/BrandController.java
@@ -2,7 +2,14 @@ package org.example.ecommerceadmin.controller;
 
 import lombok.AllArgsConstructor;
 
+import org.example.library.dto.BrandsDto;
+import org.example.library.model.Brand;
+import org.example.library.service.implement.BrandServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -10,7 +17,53 @@ import java.util.List;
 @CrossOrigin("*")
 @RestController
 @AllArgsConstructor
-@RequestMapping("/e-comAdmin/brands")
+@RequestMapping("/e-comAdmin/brand")
 public class BrandController {
+
+    @Autowired
+    private BrandServiceImpl brandService;
+
+    //    Add new brand
+    @PostMapping
+    public ResponseEntity<BrandsDto> createBrand(@RequestBody BrandsDto brandsDto,
+                                                 @RequestParam("imageBrand") MultipartFile image) {
+        BrandsDto saveBrand = brandService.createBrand(brandsDto, image);
+        return new ResponseEntity<>(saveBrand, HttpStatus.CREATED);
+    }
+
+
+    //    Get all brand
+    @GetMapping("/getAllBrand")
+    public ResponseEntity<List<BrandsDto>> getAllBrand() {
+        List<BrandsDto> brandsDto = brandService.getAllBrand();
+        return ResponseEntity.ok(brandsDto);
+    }
+
+
+    //    Get brand by id
+    @GetMapping("{brandId}")
+    public ResponseEntity<BrandsDto> getBrand(@PathVariable("brandId") Long brandId) {
+        BrandsDto brandsDto = brandService.getBrandById(brandId);
+        return ResponseEntity.ok(brandsDto);
+    }
+
+
+    //    Update brand
+    @PutMapping("{brandId}")
+    public ResponseEntity<BrandsDto> updateBrand(@RequestBody BrandsDto brandsDto,
+                                                 @PathVariable("brandId") Long id,
+                                                 @RequestParam("imageBrand") MultipartFile image) {
+        BrandsDto saveBrand = brandService.updateBrand(id, brandsDto, image);
+        return ResponseEntity.ok(saveBrand);
+    }
+
+
+    //    Delete brand
+    @DeleteMapping("{brandId}")
+    public ResponseEntity<String> deleteBrand(@PathVariable("brandId") Long id) {
+        brandService.deleteBrand(id);
+        return ResponseEntity.ok("Delete brand successfully");
+    }
+
 
 }
--- TuneBox/Library/src/main/java/org/example/library/service/implement/BrandServiceImpl.java
+++ TuneBox/Library/src/main/java/org/example/library/service/implement/BrandServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Base64;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,15 +30,15 @@ public class BrandServiceImpl implements BrandService {
     public BrandsDto createBrand(BrandsDto brandsDto, MultipartFile image) {
         try {
             Brand brand = BrandMapper.maptoBrand(brandsDto);
-            if(image == null){
+            if (image == null) {
                 brand.setBrandImage(null);
-            }else{
+            } else {
                 imageUploadBrand.uploadFile(image);
                 brand.setBrandImage(Base64.getEncoder().encodeToString(image.getBytes()));
             }
             brand.setStatus(true);
             Brand saveBrand = brandRepository.save(brand);
-            return  BrandMapper.maptoBrandsDto(saveBrand);
+            return BrandMapper.maptoBrandsDto(saveBrand);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
@@ -60,14 +61,26 @@ public class BrandServiceImpl implements BrandService {
 
     @Override
     public BrandsDto updateBrand(Long id, BrandsDto brandsDto, MultipartFile image) {
-        Brand brand = brandRepository.findById(id).orElseThrow(
-                () -> new RuntimeException("Brand not found")
-        );
-
-        brand.setName(brandsDto.getName());
-        brand.setStatus(true);
-        Brand saveBrand = brandRepository.save(brand);
-        return BrandMapper.maptoBrandsDto(saveBrand);
+        try {
+            Brand brand = brandRepository.findById(id).orElseThrow(
+                    () -> new RuntimeException("Brand not found")
+            );
+            if (image.getBytes().length > 0) {
+                if (imageUploadBrand.checkExist(image)) {
+                    brand.setBrandImage(brand.getBrandImage());
+                } else {
+                    imageUploadBrand.uploadFile(image);
+                    brand.setBrandImage(Base64.getEncoder().encodeToString(image.getBytes()));
+                }
+            }
+            brand.setName(brandsDto.getName());
+            brand.setStatus(true);
+            Brand saveBrand = brandRepository.save(brand);
+            return BrandMapper.maptoBrandsDto(saveBrand);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
