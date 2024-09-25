03b20cd73e7e7ea1e6246c6b7014d8a73ff4e8e6
Your Name <you@example.com>
2024-09-25 14:05:59 +0700

    a Nguyen Quoc Trung want toi day cai nay len de a ta su dung

--- TuneBox/EcommerceAdmin/src/main/java/org/example/ecommerceadmin/controller/BrandController.java
+++ TuneBox/EcommerceAdmin/src/main/java/org/example/ecommerceadmin/controller/BrandController.java
@@ -1,14 +1,7 @@
 package org.example.ecommerceadmin.controller;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import org.example.library.dto.BrandsDto;
-import org.example.library.model.Brand;
-import org.example.library.service.BrandService;
-import org.example.library.service.implement.BrandServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,44 +13,4 @@ import java.util.List;
 @RequestMapping("/e-comAdmin/brands")
 public class BrandController {
 
-    @Autowired
-    public BrandService brandService;
-
-
-//    Add brand
-    @PostMapping
-    public ResponseEntity<BrandsDto> createBrand(@RequestBody BrandsDto brandsDto){
-        BrandsDto saveBrand = brandService.createBrand(brandsDto);
-        return new ResponseEntity<>(saveBrand, HttpStatus.CREATED);
-    }
-
-
-//    Get all brand
-    @GetMapping("/getAllBrand")
-    public  ResponseEntity<List<BrandsDto>> getAllBrands(){
-        List<BrandsDto> brandsDto = brandService.getAllBrand();
-        return ResponseEntity.ok(brandsDto);
-    }
-
-//    Get brand by id
-    @GetMapping("{brandId}")
-    public ResponseEntity<BrandsDto> getBrandById( @PathVariable("brandId") Long id){
-    BrandsDto brandsDto = brandService.getBrandById(id);
-    return ResponseEntity.ok(brandsDto);
-    }
-
-//    Update brand
-    @PutMapping("{brandId}")
-    public ResponseEntity<BrandsDto> updateBrand(@PathVariable("brandId") Long id, @RequestBody BrandsDto brandsDto){
-        BrandsDto saveBrand = brandService.updateBrand(id, brandsDto);
-        return  ResponseEntity.ok(saveBrand);
-    }
-
-
-//    Change status brand
-    @DeleteMapping("{brandId}")
-    public ResponseEntity<String> deleteBrand(@PathVariable("brandId") Long id){
-        brandService.deleteBrand(id);
-        return ResponseEntity.ok("Delete brand successfully");
-    }
 }
