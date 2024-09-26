4d06850f4c88f5a80911f7273dc76650a148a186
halayra <quoctrung25112019@gmail.com>
2024-09-26 19:26:22 +0700

    lysm

--- TuneBox/Customer/src/main/java/org/example/customer/Controller/PostController.java
+++ TuneBox/Customer/src/main/java/org/example/customer/Controller/PostController.java
@@ -5,22 +5,33 @@ import org.example.library.dto.PostDto;
 import org.example.library.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
 @AllArgsConstructor
+@RequestMapping("API")
 public class PostController {
     private PostService postService;
 
     //create Post
-    @PostMapping("/posts")
+    @PostMapping("/post")
     public ResponseEntity<PostDto> createPost(
             @RequestPart("postDto") PostDto postDto,
             @RequestPart("images") MultipartFile[] images) {
         PostDto savedPost = postService.savePost(postDto, images);
         return new ResponseEntity<>(savedPost, HttpStatus.CREATED);
+    }
+    //get Post
+    @GetMapping("/get")
+    public ResponseEntity<PostDto> getPost(Model model, @RequestParam("id") Long id) {
+        List<PostDto> postList = postService.getAllPosts();
+        model.addAttribute("postList", postList);
+        return new ResponseEntity<>(postList.get(0), HttpStatus.OK);
     }
 
 }
