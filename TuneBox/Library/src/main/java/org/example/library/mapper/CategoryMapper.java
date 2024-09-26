11d6d741900a3f377812e6a3a0b32c6ced19a4f7
halayra <quoctrung25112019@gmail.com>
2024-09-26 12:29:44 +0700

    lysm

---
+++ TuneBox/Library/src/main/java/org/example/library/mapper/CategoryMapper.java
@@ -0,0 +1,16 @@
+package org.example.library.mapper;
+
+
+import org.example.library.dto.CategoryDto;
+import org.example.library.model.CategoryIns;
+
+public class CategoryMapper {
+
+    public static CategoryDto mapperCategoryDto(CategoryIns category) {
+        return new CategoryDto(category.getId(), category.getName(), category.isStatus());
+    }
+
+    public static CategoryIns mapperCategory(CategoryDto categoryDto) {
+        return new CategoryIns(categoryDto.getId(), categoryDto.getName(), categoryDto.isStatus());
+    }
+}
