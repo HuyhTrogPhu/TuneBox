fe12ae4d1764159d0e1d408a0965d3c7046a7392
gianhu201 <trangianhu184@gmail.com>
2024-10-06 00:39:32 +0700

    commit

--- TuneBox/Customer/src/main/java/org/example/customer/controller/UserController.java
+++ TuneBox/Customer/src/main/java/org/example/customer/controller/UserController.java
@@ -2,7 +2,6 @@ package org.example.customer.Controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import org.example.library.dto.ChangePasswordRequestDto;
 import org.example.library.dto.RequestSignUpModel;
 import org.example.library.dto.UserDto;
 import org.example.library.model.RespondModel;
@@ -12,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -25,21 +23,21 @@ import java.util.Map;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    @Autowired
-    private UserService UserService;
 
     @Autowired
-    private UserRepository Repo;
+    private UserService userService;
 
-private BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    private UserRepository userRepository;
+
+
 
     //LOGIN
     @PostMapping("/log-in")
     public ResponseEntity<?> login(@RequestBody UserDto user) {
         Map<String, Object> response = new HashMap<>();
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
         try {
-            UserDto loggedInUser = UserService.Login(user);
+            UserDto loggedInUser = userService.Login(user);
             response.put("status", true);
             response.put("message", "Đăng nhập thành công");
             response.put("data", loggedInUser);
@@ -55,9 +53,8 @@ private BCryptPasswordEncoder passwordEncoder;
     //FORGOT PASSWORD
     @PostMapping("/forgot-password")
     public ResponseEntity<String> forgotPassword(@RequestBody  UserDto user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
         try {
-            UserService.ForgotPassword(user);
+            userService.ForgotPassword(user);
             return ResponseEntity.ok("Email reset password đã được gửi đến địa chỉ của bạn");
         } catch (RuntimeException ex) {
             return ResponseEntity.badRequest().body(ex.getMessage());
@@ -67,26 +64,9 @@ private BCryptPasswordEncoder passwordEncoder;
     // RESET PASSWORD
     @PostMapping("/reset-password")
     public ResponseEntity<?> resetPassword(@RequestBody UserDto user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
         try {
-            UserService.resetPassword(user.getToken(), user.getNewPassword());
+            userService.resetPassword(user.getToken(), user.getNewPassword());
             return ResponseEntity.ok("Mật khẩu đã được đặt lại thành công");
-        } catch (RuntimeException ex) {
-            return ResponseEntity.badRequest().body(ex.getMessage());
-        }
-    }
-//Change Password
-    @PostMapping("/change-password")
-    public ResponseEntity<String> changePassword(@RequestBody ChangePasswordRequestDto changePasswordRequestdto) {
-
-        //encode
-        changePasswordRequestdto.setOldPassword(passwordEncoder.encode(changePasswordRequestdto.getOldPassword()));
-        changePasswordRequestdto.setNewPassword(passwordEncoder.encode(changePasswordRequestdto.getNewPassword()));
-        try {
-            UserService.changePassword(changePasswordRequestdto.getEmail(),
-                    changePasswordRequestdto.getOldPassword(),
-                    changePasswordRequestdto.getNewPassword());
-            return ResponseEntity.ok("Mật khẩu đã được thay đổi thành công");
         } catch (RuntimeException ex) {
             return ResponseEntity.badRequest().body(ex.getMessage());
         }
@@ -95,10 +75,10 @@ private BCryptPasswordEncoder passwordEncoder;
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable Long id) {
         try {
-            if (!Repo.existsById(id)) {
+            if (!userRepository.existsById(id)) {
                 return ResponseEntity.badRequest().body("Không tìm thấy người dùng với ID này.");
             }
-            Repo.deleteById(id);
+            userRepository.deleteById(id);
             return ResponseEntity.ok("Người dùng đã được xóa thành công.");
         } catch (Exception ex) {
             return ResponseEntity.status(500).body("Lỗi khi xóa người dùng: " + ex.getMessage());
@@ -106,47 +86,45 @@ private BCryptPasswordEncoder passwordEncoder;
     }
 
 
-//    @GetMapping("/login/oauth2/success")
-//    public ResponseEntity<?> loginWithGoogle(Authentication authentication) {
-//        Map<String, Object> response = new HashMap<>();
-//
-//        if (authentication == null || !(authentication.getPrincipal() instanceof OAuth2User)) {
-//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authentication is required");
-//        }
-//
-//        OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
-//
-//        // Kiểm tra oauth2User có phải null hay không
-//        if (oauth2User == null) {
-//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("OAuth2 User is not found");
-//        }
-//
-//        String email = oauth2User.getAttribute("email");
-//        String name = oauth2User.getAttribute("name");
-//
-//        if (email == null || name == null) {
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email or name not found");
-//        }
-//
-//        UserDto userDto = UserService.loginWithGoogle(email, name);
-//
-//        response.put("status", true);
-//        response.put("message", "Đăng nhập thành công");
-//        response.put("data", userDto);
-//
-//        return ResponseEntity.ok(response);
-//    }
+    @GetMapping("/login/oauth2/success")
+    public ResponseEntity<?> loginWithGoogle(Authentication authentication) {
+        Map<String, Object> response = new HashMap<>();
+
+        if (authentication == null || !(authentication.getPrincipal() instanceof OAuth2User)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authentication is required");
+        }
+
+        OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
+
+        // Kiểm tra oauth2User có phải null hay không
+        if (oauth2User == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("OAuth2 User is not found");
+        }
+
+        String email = oauth2User.getAttribute("email");
+        String name = oauth2User.getAttribute("name");
+
+        if (email == null || name == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email or name not found");
+        }
+
+        UserDto userDto = userService.loginWithGoogle(email, name);
+
+        response.put("status", true);
+        response.put("message", "Đăng nhập thành công");
+        response.put("data", userDto);
+
+        return ResponseEntity.ok(response);
+    }
 
     @PostMapping("/sign-up")
     public ResponseEntity<?> Register(@RequestBody RequestSignUpModel requestSignUpModel,
                                       HttpServletRequest request
     ) {
-        String psEncode =passwordEncoder.encode(requestSignUpModel.getUserDto().getPassword());
-        requestSignUpModel.getUserDto().setPassword(psEncode);
         RespondModel response = new RespondModel();
         try {
             response.setMessage("Đăng ký thành công");
-            response.setData(UserService.Register(requestSignUpModel));
+            response.setData(userService.Register(requestSignUpModel));
             response.setStatus(true);
             HttpSession session = request.getSession(true);
             session.setAttribute("userId", requestSignUpModel.getUserDto().getId());
@@ -164,7 +142,8 @@ private BCryptPasswordEncoder passwordEncoder;
         try {
             response.put("status", true);
             response.put("message", "Succesfull");
-            response.put("data", UserService.findById(UserId));
+            response.put("data", userService.findById(UserId));
+
         } catch (Exception ex) {
             response.put("status", false);
             response.put("message", "Fail");
@@ -200,11 +179,25 @@ private BCryptPasswordEncoder passwordEncoder;
     @PostMapping("/check")
     public ResponseEntity<String> check(@RequestBody RequestSignUpModel requestSignUpModel) {
         try {
-            UserService.CheckLogin(requestSignUpModel);
+            userService.CheckLogin(requestSignUpModel);
             return ResponseEntity.ok("Check completed successfully");
         } catch (ResponseStatusException ex) {
             return ResponseEntity.status(ex.getStatusCode()).body(ex.getReason()); // Use getStatusCode()
         }
     }
-
+    // Lấy thông tin người dùng hiện tại
+    @GetMapping("/current")
+    public ResponseEntity<UserDto> getCurrentUser(@RequestParam("userId") Long userId) {
+        if (userId != null) {
+            UserDto userDto = userService.getUserById(userId);
+            return ResponseEntity.ok(userDto);
+        }
+        return ResponseEntity.status(401).body(null); // Trả về 401 nếu không tìm thấy thông tin người dùng
+    }
+    // Lấy thông tin người dùng theo ID
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserById(@RequestParam("userId") Long userId) {
+        UserDto userDto = userService.getUserById(userId);
+        return ResponseEntity.ok(userDto);
+    }
 }
