6ccd998ac48332269e236e4041812dad36d8278d
Your Name <you@example.com>
2024-10-24 14:30:30 +0700

    tham hoa

--- TuneBox/Library/src/main/java/org/example/library/dto/MessageDto.java
+++ TuneBox/Library/src/main/java/org/example/library/dto/MessageDto.java
@@ -1,33 +1,26 @@
 package org.example.library.dto;
 
-
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.example.library.model.Brand;
-import org.example.library.model.CategoryIns;
-import org.example.library.model.Chat;
 import org.example.library.model.User;
+import org.example.library.service.UserService;
 
 import java.time.LocalDateTime;
 
+
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
-public class MessageDto {
-
+@AllArgsConstructor
+public class MessageDTO {
     private Long id;
-
-    private String message;
-
-    private LocalDateTime dateTime;
-
-    private Chat chat;
-
-    private User user;
-
+    private String content;
+    private LocalDateTime creationDate;
+    private Long senderId;
+    private Long receiverId;
+    private Long chatId;
 }
