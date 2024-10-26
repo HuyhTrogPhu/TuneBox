91aaab222639a4da002004543f6e379a979c93f9
gianhu201 <trangianhu184@gmail.com>
2024-10-26 12:39:19 +0700

    oke

--- TuneBox/Customer/src/main/java/org/example/customer/controller/UserController.java
+++ TuneBox/Customer/src/main/java/org/example/customer/controller/UserController.java
@@ -7,7 +7,6 @@ import org.example.library.dto.*;
 import org.example.library.model.Genre;
 import org.example.library.model.InspiredBy;
 import org.example.library.model.Talent;
-import org.example.library.model.UserInformation;
 import org.example.library.repository.UserRepository;
 import org.example.library.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,14 +19,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.http.MediaType;
 
 
 @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
@@ -40,6 +34,9 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private UserInformationService userInformationService;
 
     @Autowired
     private TalentService talentService;
@@ -192,166 +189,29 @@ public class UserController {
         }
     }
 
-    // get user in profile page
+    // get user information in profile page
     @GetMapping("/{userId}/settingProfile")
     public ResponseEntity<ProfileSettingDto> getUserInformation(@PathVariable Long userId) {
         try {
-            ProfileSettingDto profileUser = userService.getUserProfileSetting(userId);
-            return ResponseEntity.ok(profileUser);
+            ProfileSettingDto userInfo = userInformationService.getUserInformation(userId);
+            return ResponseEntity.ok(userInfo);
         } catch (Exception e) {
             e.printStackTrace();
             return (ResponseEntity<ProfileSettingDto>) ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-
-    // get user in account page
-    @GetMapping("/{userId}/accountSetting")
-    public ResponseEntity<AccountSettingDto> getUserAccount(@PathVariable Long userId) {
-        try {
-            AccountSettingDto userAccount = userService.getAccountSetting(userId);
-            return ResponseEntity.ok(userAccount);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return (ResponseEntity<AccountSettingDto>) ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
     // get follower and following user by user id
     @GetMapping("/{userId}/followCount")
-    public ResponseEntity<Map<String, Long>> getFollowCount(@PathVariable Long userId) {
+    public ResponseEntity<Optional<UserFollowDto>> getFollowCount(@PathVariable Long userId) {
         try {
-            Long followersCount = userService.getFollowersCount(userId);
-            Long followingCount = userService.getFollowingCount(userId);
-            Map<String, Long> followCounts = new HashMap<>();
-            followCounts.put("followers", followersCount);
-            followCounts.put("following", followingCount);
-            return ResponseEntity.ok(followCounts);
+            Optional<UserFollowDto> followCount = userService.getUserFollowById(userId);
+            return ResponseEntity.ok(followCount);
         } catch (Exception e) {
             e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-        }
-    }
-
-    // set password in account page
-    @PutMapping("/{userId}/password")
-    public ResponseEntity<String> setPassword(@PathVariable Long userId, @RequestParam String newPassword) {
-        try {
-            String encodedNewPassword = passwordEncoder.encode(newPassword);
-
-            userService.setPassword(userId, encodedNewPassword);
-            return ResponseEntity.ok("Mật khẩu đã thay đổi thành công");
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error updating password");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Optional.empty());
         }
     }
 
 
-    // set username in account page
-    @PutMapping("/{userId}/username")
-    public ResponseEntity<String> setUsername(@PathVariable Long userId, @RequestParam String newUsername) {
-        try {
-            userService.updateUserName(userId, newUsername);
-            return ResponseEntity.ok("Tên đăng nhập đã thay đổi thành công");
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error updating username");
-        }
     }
-
-    // set email in account page
-    @PutMapping("/{userId}/email")
-    public ResponseEntity<String> setEmail(@PathVariable Long userId, @RequestBody EmailUpdateRequest emailUpdateRequest) {
-        try {
-            userService.updateEmail(userId, emailUpdateRequest.getNewEmail()); // Sử dụng getNewEmail từ DTO
-            return ResponseEntity.ok("Email đã thay đổi thành công");
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error updating email");
-        }
-    }
-    // Cập nhật ngày sinh
-    @PutMapping("/{userId}/birthday")
-    public ResponseEntity<String> setBirthday(@PathVariable Long userId, @RequestBody String newBirthday) {
-        try {
-            // Làm sạch chuỗi ngày sinh bằng cách loại bỏ dấu nháy đôi
-            newBirthday = newBirthday.replace("\"", "").trim();
-
-            // Chuyển đổi chuỗi thành Date
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-            Date birthday = dateFormat.parse(newBirthday);
-
-            userService.updateBirthday(userId, birthday); // Cập nhật ngày sinh
-            return ResponseEntity.ok("Ngày sinh đã thay đổi thành công");
-        } catch (ParseException e) {
-            e.printStackTrace();
-            return ResponseEntity.badRequest().body("Ngày sinh không hợp lệ");
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error updating birthday");
-        }
-    }
-    // Cập nhật giới tính
-    @PutMapping("/{userId}/gender")
-    public ResponseEntity<String> setGender(@PathVariable Long userId, @RequestBody String newGender) {
-        try {
-            String sanitizedGender = newGender.replace("\"", ""); // Xóa dấu ngoặc kép
-            userService.updateGender(userId, sanitizedGender);
-            return ResponseEntity.ok("Giới tính đã thay đổi thành công");
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error updating gender");
-        }
-    }
-
-    //Cập nhật thông tin người dùng
-    @PutMapping(value = "/{userId}/update", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> updateUserInfo(
-            @PathVariable Long userId,
-            @RequestBody UserUpdateRequest userUpdateRequest) {
-        System.out.println("Received request: " + userUpdateRequest);
-        try {
-            // Kiểm tra và cập nhật username nếu nó không null
-            if (userUpdateRequest.getUserName() != null) {
-                userService.updateUserName(userId, userUpdateRequest.getUserName());
-            }
-
-            // Cập nhật thông tin người dùng
-            if (userUpdateRequest.getUserInformation() != null) {
-                UserInfoUpdateDto userInfo = userUpdateRequest.getUserInformation();
-                // Chỉ cập nhật name nếu nó không null
-                if (userInfo.getName() != null) {
-                    userService.updateUserInformation(userId, userInfo.getName(),
-                            userInfo.getLocation() != null ? userInfo.getLocation() : null,
-                            userInfo.getAbout() != null ? userInfo.getAbout() : null);
-                }
-
-                // Cập nhật location nếu nó không null
-                if (userInfo.getLocation() != null) {
-                    userService.updateUserInformation(userId,
-                            userInfo.getName() != null ? userInfo.getName() : null,
-                            userInfo.getLocation(),
-                            userInfo.getAbout() != null ? userInfo.getAbout() : null);
-                }
-
-                // Cập nhật about nếu nó không null
-                if (userInfo.getAbout() != null) {
-                    userService.updateUserInformation(userId,
-                            userInfo.getName() != null ? userInfo.getName() : null,
-                            userInfo.getLocation() != null ? userInfo.getLocation() : null,
-                            userInfo.getAbout());
-                }
-            }
-
-            return ResponseEntity.ok("Cập nhật thông tin thành công!");
-        } catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        } catch (Exception e) {
-            e.printStackTrace(); // Để có thêm thông tin nếu có lỗi
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Đã xảy ra lỗi khi cập nhật thông tin.");
-        }
-    }
-
-
-}
