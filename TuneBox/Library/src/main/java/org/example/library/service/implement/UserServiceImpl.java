e99633b086966b9961d8cd46c3ab25ed33baefb6
Pham Xuan Truong <122740745+sonnpxt04@users.noreply.github.com>
2024-11-07 13:56:59 +0700

    mới nhất 11.7 v2

--- TuneBox/Library/src/main/java/org/example/library/service/implement/UserServiceImpl.java
+++ TuneBox/Library/src/main/java/org/example/library/service/implement/UserServiceImpl.java
@@ -72,7 +72,7 @@ public class UserServiceImpl implements UserService {
 
             user.setUserInformation(userInformation);
 
-            user.setRole(roleRepository.findByName("ECOMADMIN"));
+            user.setRole(roleRepository.findByName("SocialAdmin"));
             user.setEmail(userDto.getEmail());
             user.setUserName(userDto.getUserName());
             user.setPassword(userDto.getPassword());
