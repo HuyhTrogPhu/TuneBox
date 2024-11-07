313a13ea0bd5b2c7b71df928310944c50f287de9
halayra <quoctrung25112019@gmail.com>
2024-11-07 12:40:46 +0700

    cap nhat truoc khi merge Phong

--- TuneBox/Library/src/main/java/org/example/library/service/implement/UserServiceImpl.java
+++ TuneBox/Library/src/main/java/org/example/library/service/implement/UserServiceImpl.java
@@ -50,6 +50,7 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserInformationRepository userInformationRepository;
 
+
     @Override
     public UserDto register(UserDto userDto, UserInformationDto userInformationDto, MultipartFile image) {
         try {
@@ -69,7 +70,7 @@ public class UserServiceImpl implements UserService {
 
             user.setUserInformation(userInformation);
 
-            user.setRole(roleRepository.findByName("CUSTOMER"));
+            user.setRole(roleRepository.findByName("ECOMADMIN"));
             user.setEmail(userDto.getEmail());
             user.setUserName(userDto.getUserName());
             user.setPassword(userDto.getPassword());
@@ -218,7 +219,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public AccountSettingDto getAccountSetting(Long userId) {
-        return userRepository.findAccountSettingProfile(userId);
+       return userRepository.findAccountSettingProfile(userId);
     }
 
     @Override
