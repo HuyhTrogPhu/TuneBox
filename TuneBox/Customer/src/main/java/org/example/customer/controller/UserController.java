d81afd785a1d9abf7d661d1043be651f19a2e073
halayra <quoctrung25112019@gmail.com>
2024-11-15 19:54:40 +0700

    lasttttt

--- TuneBox/Customer/src/main/java/org/example/customer/controller/UserController.java
+++ TuneBox/Customer/src/main/java/org/example/customer/controller/UserController.java
@@ -90,6 +90,7 @@ public class UserController {
     }
 
 
+
     // get list talents
     @GetMapping("/list-talent")
     public ResponseEntity<List<Talent>> listTalent() {
@@ -188,6 +189,7 @@ public class UserController {
 
         return ResponseEntity.ok().build();
 
+    }
 
     // Phương thức để lấy userId từ cookie
     private String getUserIdFromCookie(HttpServletRequest request) {
