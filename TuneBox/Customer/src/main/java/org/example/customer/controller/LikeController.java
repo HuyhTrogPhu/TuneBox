f014e635cb6dad8e8b2b8031609ce0749d3a41bb
phamhoaiphong-dev <phongphdev@gmail.com>
2024-12-12 11:10:41 +0700

    .

--- TuneBox/Customer/src/main/java/org/example/customer/controller/LikeController.java
+++ TuneBox/Customer/src/main/java/org/example/customer/controller/LikeController.java
@@ -77,9 +77,6 @@ public class LikeController {
         }
     }
 
-
-
-
     @DeleteMapping("/remove")
     public ResponseEntity<Void> removeLike(@RequestParam Long userId,
                                            @RequestParam(required = false) Long postId,
